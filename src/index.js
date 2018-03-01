646fe789728fc263e3bb08bfce921702ba1db1e4
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2018-03-01 15:19:00 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,39 +1,40 @@
 
 module.exports = function makeExchange(currency) {
-//var H = 50, Q = 25, D = 10,N = 5, P = 1;
+
 var coins = [50,25,10,5,1];
-var change = [];
-var letters = ["H:","Q:","D:","N:","P:"];
-var i = 0, res = 1;
-if(currency>1000)
+var coins_1 = {H:null, Q:null, D:null, N:null, P:null};
+var change = {};
+var i = 0,k = 0, res = 1;
+if(currency>10000)
 change = {error: "You are rich, my friend! We don't have so much coins for exchange"};
 else
 {
 if(currency<=0)
 
 change = {};
+
  else {
-    while(res){
-    if(currency<coins[i])
-    i++;
-    else
-    {
-       change[i] = letters[i] + currency/coins[i];
-       res = currency%coins[i];}
 
 
-    }
- //change[0]='H:' +change[0];
- //change[1]='Q:' +change[1];
- //change[2]='D:' +change[2];
- //change[3]='N:' +change[3];
- //change[4]='P:' +change[4];
+        for(var key in coins_1)
+        {
+        if(currency<coins[i])
+        {
+        i++;
+        continue;
+        }
+       change[key] = Math.floor(currency/coins[i]);
+       currency = currency - Math.floor(currency/coins[i])*coins[i];
+        i++;
+        if(!currency) break;
+         }
+
+
+
  }
 
  }
 
-  console.log('----------------------------------------');
- console.log(change);
+
     return change;
 }
-
